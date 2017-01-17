6033c24c324e142fce68533bda2c4f86910618a5
vasanth <vasanth.raveendran@cooltrax.com>
2017-01-18 02:17:24 +1100

    movie detail implemented

---
+++ Controllers/MovieDetailController.cs
@@ -0,0 +1,22 @@
+﻿using Movies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Movies.Controllers
+{
+    public class MovieDetailController : Controller
+    {
+        // GET: Movie
+        
+        public ActionResult Index(string id, string movieProvider)
+        {
+            Movie movie = new Movie();
+            movie = Common.Common.GetMovieFromWebAPI(id, movieProvider);
+            
+            return View(movie);
+        }
+    }
+}
---
+++ Models/RootObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Movies.Models
+{
+    public class RootObject : List<Movie>
+    {
+        public List<Movie> listOfMovies { get; set; }
+    }
+}
